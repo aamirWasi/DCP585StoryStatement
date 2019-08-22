171751be79c69a4f17a3aad13a538a31b906f94c
aamir <wasi3aamir@gmail.com>
2019-08-22 14:04:02 +0600

    back to back wrong answer

---
+++ StoryStatement/DCP500WinkKick.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace StoryStatement
+{
+    public class DCP500WinkKick
+    {
+        static void Main()
+        {
+            ulong T = ulong.Parse(Console.ReadLine());
+            for (ulong t = 1; t <= T; t++)
+            {
+                string input = Console.ReadLine();
+                var inputArray = input.Split(' ');
+                
+                ulong num1 = ulong.Parse(inputArray[0]);
+                ulong num2 = ulong.Parse(inputArray[1]);
+                if (num2 != 0)
+                {
+                    var result = num1 % num2;
+                    if (result == 0 && num1!=0)
+                    {
+                        Console.WriteLine(":wink:");
+                    }
+                    else
+                        Console.WriteLine(":kick:");
+                }
+            }
+        }
+    }
+}
--- StoryStatement/Program.cs
+++ StoryStatement/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace StoryStatement
@@ -9,6 +11,16 @@ namespace StoryStatement
     {
         static void Main(string[] args)
         {
+            string text = Console.ReadLine();
+            //bool result = Regex.IsMatch(text, "\\programming\\b", RegexOptions.IgnoreCase);
+            if (text.Contains("programming"))
+            {
+                Console.WriteLine("TRUE");
+            }
+            else
+            {
+                Console.WriteLine("FALSE");
+            }
         }
     }
 }
